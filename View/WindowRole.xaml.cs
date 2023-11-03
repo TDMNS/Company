ff30d5593f1238ce0ee1ae59c8ab9aecba31bf2e
TDMNS <773o@mail.ru>
2023-11-03 19:26:30 +0700

    Лабораторная работа №2

--- View/WindowRole.xaml.cs
+++ View/WindowRole.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using KolbasaLos.ViewModel;
 
 namespace KolbasaLos.View
 {
@@ -22,6 +11,8 @@ namespace KolbasaLos.View
         public WindowRole()
         {
             InitializeComponent();
+            RoleViewModel vmRole = new RoleViewModel();
+            lvRole.ItemsSource = vmRole.ListRole;
         }
     }
 }
